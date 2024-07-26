0097b1450a15b3015008f05082c5518527b7d7bc
Windings-Lab <janton20001@gmail.com>
2024-07-26 20:46:06 +0300

    Post request to change category

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,6 +4,24 @@ test.beforeEach(async ({ page }) => {
   await page.goto('https://www.demoblaze.com/');
 });
 
+async function postCategory(page: any, category: string)
+{
+  const pageData = await page.evaluate(async (cat: string) =>
+  {
+    const payload = { cat };
+    const response = await fetch('https://api.demoblaze.com/bycat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    });
+    return await response.json();
+  }, category);
+  const responseCategory = pageData.Items[0].cat;
+  expect(responseCategory).toBe(category);
+}
+
 async function checkCategory(page: any, expectedCategory: string)
 {
   const response = await page.waitForResponse(response =>
@@ -18,6 +36,9 @@ async function checkCategory(page: any, expectedCategory: string)
 
 test('changing category', async ({ page }) =>
 {
+  await postCategory(page, 'phone')
+  await postCategory(page, 'notebook')
+  await postCategory(page, 'monitor')
   await page.getByRole('link', { name: /Phones/i }).click()
   await checkCategory(page, 'phone')
   await page.getByRole('link', { name: /Laptops/i }).click();
